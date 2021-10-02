a5c5d3ce71c244823fd92dfc6b37e4ba98dbd2d3
snowlinreximar <cecil.snow@gmail.com>
2021-10-02 23:21:09 +0300

    Angular first commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+//import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() txt: string;
+  @Output() btnClick = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.btnClick.emit();
+  }
+
+}
